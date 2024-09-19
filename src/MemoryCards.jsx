fa795a1f8f5209cf73c57457210d8d1e8c599a29
Stephen <stephensouness@outlook.com>
2024-09-19 14:32:57 +0100

    mapped the cat images to the Memory Cards component

--- src/MemoryCards.jsx
+++ src/MemoryCards.jsx
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react";
 import MemoryCard from "./MemoryCard";
 import ScoreBoard from "./ScoreBoard";
-import fetchImages from "./api.js";
-
-fetchImages();
+import getImages from "./api.js";
+import "../App.css";
 
 function MemoryCards() {
-  return (
-    <>
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getImages().then((images) => {
+      setImages(images);
+      setLoading(false);
+    });
+  }, []);
+
+  return loading ? (
+    <p>...page is loading</p>
+  ) : (
+    <div>
       <h2>Memory Cards</h2>
+
       <ScoreBoard />
       <MemoryCard />
-    </>
+      <ul className="image-container">
+        {images.map((image) => {
+          return (
+            <li key={image.id}>
+              <p>image.height</p>
+              <img src={image.url} alt={image.breeds.name} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-async function fetchImages() {
+async function getImages() {
   try {
     const response = await fetch(
       "https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_T1LnrLnXcwvKqaSn5FrUtev693zNCbefFmLyEavTOOV046vj1ulb8koTJAV83PtZ"
@@ -9,10 +9,11 @@ async function fetchImages() {
     }
 
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
+    return [];
   }
 }
 
-export default fetchImages;
+export default getImages;
